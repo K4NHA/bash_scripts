d8ee9d6a65abad154456aaffcd3bc94ae19c6489
Brad Arnold <k4nha@k4nha.com>
2019-06-05 18:19:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# bash_scripts
+
+General BASH scripts I use quite often.
+
+
+<b>del_filelist.sh -</b> Deletes files and folders added to a text file using a function included in the file comments.
---
+++ del_filelist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Deletes files and directories in the list file after using
+# the function below.
+#
+# Add the following function to your .bash_profile (Linux) or
+# .bashrc (OSX) in order to create the list file. Then use the
+# function and it will create files_to_delete.txt in your
+# home directory. Feel free to change the name of the function
+# if it conflicts with any other commands or aliases.
+#
+# md() {
+#	#add filenames to a file to be deleted as a batch
+#	#echo realpath "$1" >> ~/files_to_delete.txt
+#	echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1") >> ~/files_to_delete.txt
+#}
+#
+
+while read -r file; do rm -r -- "$file"; done < files_to_delete.txt
